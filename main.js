bdde4301d6d425041d61874fa703f2e419f4e4e3
Alondra Warren <daisy.codeimmersives.com>
2021-03-07 15:19:41 -0800

    call my own functions

---
+++ main.js
@@ -0,0 +1,23 @@
+let result1 = ("hello" + "???");
+result1; 
+
+let result2 = ("what even" + "???");
+result2;
+
+let result3 = ("oh" + " " + "hi");
+result3;
+
+let result4 = ("well" + " " + "hello back");
+result4
+
+let result5 = (10 - 1 - 2);
+result5; 
+
+let result6 = (2 - 1 - 10);
+result6;
+
+let result7 = ("codeimmersives" + "codeimmersives" + "codeimmersives");
+result7;
+
+let squareRoot = (16/4);
+squareRoot; 
